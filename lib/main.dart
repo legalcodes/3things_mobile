5fc0ff21b3ead6cb6e54593208a1ddcf168fbbb8
Jon Tippens <jtippens@jtippens-macbookpro.roam.corp.google.com>
2019-03-31 22:59:00 -0700

    Added quotes, fonts, and form inputs

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,123 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import "quotes.dart";
 
-void main() => runApp(new MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
-    return new MaterialApp(
-      title: 'Welcome to Startup Namer',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: const Text('Welcome to Startup Namer'),
+    return MaterialApp(
+      title: '3Things',
+      // Set AlfaSlabOne as the default app font
+      theme: ThemeData(fontFamily: 'AlfaSlabOne'),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Title(),
+            Quote(),
+            ThreeThings(),
+          ],
         ),
-        body: new Center(
-//          child: const Text('Hello World'),
-        child: new Text(wordPair.asPascalCase),
+        )
+      );
+  }
+}
+
+class Title extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.topCenter,
+      padding: EdgeInsets.only(top: 48),
+      child: RichText(
+        text: TextSpan(
+          text:'3',
+          style: TextStyle(
+            fontFamily: 'AlfaSlabOne',
+            fontSize: 70,
+            color: Colors.grey,
+          ),
+          children: <TextSpan>[
+            TextSpan(
+                text: 'THINGS',
+                style: TextStyle(
+                  color: Colors.blueGrey,
+                )
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
+class Quote extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var quote = randomQuote();
+    return Container(
+      alignment: Alignment.topCenter,
+      padding: EdgeInsets.only(left:18, right:18),
+      child: Text(
+        '${quote}',
+      ),
+    );
+  }
+}
+
+class ThreeThings extends StatefulWidget {
+  const ThreeThings({
+    Key key,
+    this.thing1: '',
+    this.thing2: '',
+    this.thing3: '',
+  }) : super(key: key);
+
+  final String thing1;
+  final String thing2;
+  final String thing3;
+
+  _ThreeThingsState createState() => _ThreeThingsState();
+}
+
+class _ThreeThingsState extends State<ThreeThings> {
+  String _thing1 = '';
+  String _thing2 = '';
+  String _thing3 = '';
+
+  void setThing1(text) {
+    setState(() { _thing1 = text; });
+  }
+  void setThing2(text) {
+    setState(() { _thing2 = text; });
+  }
+  void setThing3(text) {
+    setState(() { _thing3 = text; });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+      padding: EdgeInsets.only(left:24, right:24),
+      child: Column(
+        children: [
+          TextFormField(),
+          TextFormField(),
+          TextFormField()
+        ]
+      ),
+    );
+  }
+}
+
+
---
+++ lib/quotes.dart
@@ -0,0 +1,19 @@
+import 'dart:math';
+
+var quotes = [
+"“Beware the barrenness of a busy life. —Socrates”",
+"“Better a little which is well done, than a great deal imperfectly.” —Plato",
+"“Pleasure in the job puts perfection in the work.” —Aristotle",
+"“...do what’s essential. Do less, better. Because most of what we do or say is not essential. If you can eliminate it, you’ll have more tranquility.” —Marcus Aurelius",
+"“Neither blame or praise yourself.” —Plutarch",
+"“It does not matter how slowly you go as long as you do not stop.” —Confucius",
+"“Great acts are made up of small deeds.” —Lao Tzu",
+"“Hard choices, good life. Easy choices, hard life.” -- Jerzy Gregorek"
+];
+
+String randomQuote () {
+  var rnd = new Random();
+  int min = 0;
+  int max = quotes.length-1;
+  return quotes[rnd.nextInt(max-min)];
+}
