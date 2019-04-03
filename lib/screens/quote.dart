47f9f75c9d03db6222eeb501f98b5705d7a3f76e
Jon Tippens <jtippens@google.com>
2019-04-03 11:07:19 -0700

    Added fade in transition for quote

--- lib/screens/quote.dart
+++ lib/screens/quote.dart
@@ -2,16 +2,37 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:threethings/quotes.dart';
 
-class Quote extends StatelessWidget {
+class Quote extends StatefulWidget {
+  @override
+  _QuoteState createState() => _QuoteState();
+}
+
+class _QuoteState extends State<Quote> with SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     var quote = randomQuote();
-    return Container(
+
+    final controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2)
+    );
+
+    final animation = Tween(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(controller);
+
+    controller.forward();
+
+    return FadeTransition(
+      opacity: animation,
+     child: Container(
       alignment: Alignment.topCenter,
       padding: EdgeInsets.only(left:18, right:18),
       child: Text(
         '${quote}',
       ),
+    )
     );
   }
 }
